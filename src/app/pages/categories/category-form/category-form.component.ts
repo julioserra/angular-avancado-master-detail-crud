8973558b75c1818640cd88f07b822bd38c5557a0
Julio Serra <j.serra@itagtecnologia.com.br>
2019-01-17 11:52:34 -0200

    adding base methods category form template

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -1,15 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { CategoryService } from './../shared/category.service';
+import { Category } from './../shared/category.model';
+import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { switchMap } from "rxjs/operators";
+
+import toastr from "toastr";
 
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
   styleUrls: ['./category-form.component.css']
 })
-export class CategoryFormComponent implements OnInit {
+export class CategoryFormComponent implements OnInit, AfterContentChecked {
 
-  constructor() { }
+  currentAction: string;
+  categoryForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm: boolean = false;
+  category: Category = new Category();
+
+  constructor(
+    private categoryService: CategoryService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.setCurrentAction();
+    this.builCategoryForm();
+    this.loadCategory();
+  }
+
+  //O método é invocado depois que estiver tudo carregado,
+  //assim garente que o título da página após o carregamento.
+  ngAfterContentChecked(){
+    this.setPageTitle();
+  }
+
+  //PRIVATES METHODS
+
+  private setCurrentAction(){
+    //para saber se a pessoa está editando ou não
+    //aqui retorna um array contendo todos os seguimentos da url
+    //a partir de /categories 
+    //se tiver criando será /categories/new, se tiver editando será por exemplo
+    //categories/123/edit
+    //[0] primeiro seguinte do path
+    if(this.route.snapshot.url[0].path == "new"){
+      this.currentAction = "new"
+    } else {
+      this.currentAction = "edit"
+    }
+  }
+
+  private builCategoryForm(){
+    this.categoryForm = this.formBuilder.group({
+      id: [null],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      description: [null]
+    });
+  }
+
+  private loadCategory(){
+    if (this.currentAction == "edit"){
+      this.route.paramMap.pipe(
+        switchMap(params => this.categoryService.getById(+params.get("id")))//o mais antes de params é para fazer um cast para number.
+      )
+      .subscribe(
+        (category) => {
+          this.category = category;
+          //fazer um bind
+          this.categoryForm.patchValue(category)//binds loaded category data to CategoryForm.
+        },
+        (error) => alert('Ocorreu um erro no servidor, tente mais tarde.')
+      )
+    }
+  }
+
+  private setPageTitle(){
+    if (this.currentAction == "new"){
+      this.pageTitle = 'Cadastro de Nova Categoria'
+    }else{
+      //se ainda não estiver carregado category.name irá retornar nulo,
+      //então fica null ou vazio.
+      //então garanto que quando for nulo o category name seja preenchido com uma
+      //string vazia, assim não irá aparecer para o usuário:
+      //Editando Categoria: null.
+      const categoryName = this.category.name || ""
+      this.pageTitle = 'Editando Categoria: ' + categoryName
+    }
   }
 
 }
