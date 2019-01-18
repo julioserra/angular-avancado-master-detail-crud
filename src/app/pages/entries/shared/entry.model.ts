5ee8a413bf18031a3de66aa05268c528ef92eebd
Julio Serra <j.serra@itagtecnologia.com.br>
2019-01-18 14:18:54 -0200

    improving EntriesModule

--- src/app/pages/entries/shared/entry.model.ts
+++ src/app/pages/entries/shared/entry.model.ts
@@ -15,7 +15,7 @@ export class Entry {
 
     static types = {
         expense: 'Despesa',
-        renevue: 'Receita'
+        revenue: 'Receita'
     };
 
     //Para verificar se a despesa ou receita
